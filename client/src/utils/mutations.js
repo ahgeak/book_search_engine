96e483a91b94f553c5b556bdf214a9c1cccbd51f
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-03-03 19:26:35 -0800

    mutations.js: update saveBook and removeBook mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,50 @@
-import {gql} from '@apollo/client'
+import { gql } from "@apollo/client";
 
+// LOGIN_USER will execute the loginUser mutation
 export const LOGIN_USER = gql`
-mutation login($email: String, $password: String) {
-  login( email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
-      email
+  mutation login($email: String, $password: String) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
   }
-}`
+`;
 
+// ADD_USER will execute the addUser mutation
 export const ADD_USER = gql`
-mutation addUser($username: String, $email: String, $password: String) {
-  addUser(username: $username, email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
-      email
+  mutation addUser($username: String, $email: String, $password: String) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
   }
-}
-`
+`;
+// SAVE_BOOK will execute the saveBook mutation
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInput: BookInput) {
+    saveBook(bookInput: $bookInput) {
+      token
+      _id
+      username
+    }
+  }
+`;
+
+// REMOVE_BOOK will execute the removeBook mutation
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID) {
+    removeBook(bookId: $bookId) {
+      token
+      _id
+      username
+    }
+  }
+`;
